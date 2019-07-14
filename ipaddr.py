604865a16b10e7c00d579f597d00d473b61ebcc3
Shreyas Ghorpade <36772206+ShreyasG1709@users.noreply.github.com>
2019-07-14 18:23:10 +0530

    Update ipaddr.py

--- ipaddr.py
+++ ipaddr.py
@@ -1,8 +1,11 @@
 ipaddr=input("Enter an IP Add")
+if ipaddr[-1] != '.': # drop these 2 lines and undo all# for alternate way, confirming segment initially
+ ipaddr += '.' 
+
 
 segment=1
 seg_len=0
-char=""
+ #char = ""
 
 for char in ipaddr:
  if char == '.':
@@ -12,5 +15,5 @@ for char in ipaddr:
  else: 
    seg_len+=1
    
-if char != '.': 
- print("segment {} contains {} characters".format(segment, seg_len))
+#if char != '.': 
+ #print("segment {} contains {} characters".format(segment, seg_len))
