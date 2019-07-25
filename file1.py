c54186bfbc48feacde2cb0f154326ecd0f026554
Shreyas Ghorpade <36772206+ShreyasG1709@users.noreply.github.com>
2019-07-25 22:45:47 +0530

    file reding and printing in normal and reverse form
    printed in lines reverse and char reverse

---
+++ file1.py
@@ -0,0 +1,34 @@
+with open("/Users/Shreyas/Desktop/file.txt",'r') as a:
+ for line in a:
+   print(line, end='')
+
+print("*"*40)
+
+with open("/Users/Shreyas/Desktop/file.txt",'r') as a:
+ lines = a.readlines()
+print(lines, end='')
+
+print("*"*40) 
+
+for line in lines[::-1]:
+ print(line)
+ 
+
+print("*"*40)
+
+
+with open("/Users/Shreyas/Desktop/file.txt",'r') as a:
+ lines = a.read()
+
+for line in lines[::-1]:
+ print(line, end='')
+print("")
+
+print("#"*40)
+
+with open("/Users/Shreyas/Desktop/file.txt",'r') as a:
+ lines = a.readlines()
+
+for line in lines:
+ for char in line[::-1]:
+  print(char, end='')
