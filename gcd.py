8e358ff981addebd043984f93b94e3a85439c2b4
Shreyas Ghorpade <36772206+ShreyasG1709@users.noreply.github.com>
2019-07-30 22:42:53 +0530

    Updated GCD

--- gcd.py
+++ gcd.py
@@ -1,19 +1,9 @@
 def gcd(m,n):
 
- fm=[]
- for i in range(1,m+1):
-  if(m%i==0):
-   fm.append(i)
-  
- fn=[]
- for j in range(1,n+1):
-  if(n%j==0):
-   fn.append(j)
-
  cf=[]
- for f in fm:
-  if f in fn:
-   cf.append(f)
+ for i in range(1,min(m,n)+1):
+  if m%i==0 and n%i==0:
+   cf.append(i)
 
  print(cf[-1])
 
