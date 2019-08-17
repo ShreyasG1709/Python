a2c3a0a22b9a13a99e14d40b3d1754c5ec22cda9
Shreyas Ghorpade <36772206+ShreyasG1709@users.noreply.github.com>
2019-08-17 12:58:42 +0530

    Insertion sort

---
+++ py2.py
@@ -0,0 +1,15 @@
+def insort(nlist):
+ for index in range(1,len(nlist)):
+  cv=nlist[index]
+  pos=index
+
+  while pos>0 and nlist[pos-1]>cv:
+   nlist[pos]=nlist[pos-1]
+   pos-=1
+
+  nlist[pos]=cv
+
+nlist=[12,11,13,5,6]
+insort(nlist)
+
+print(nlist)
